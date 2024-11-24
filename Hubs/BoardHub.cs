af401fbb958cb2541f586a259a516ac984be933b
Sankarrao Kolli <Sankarrao.Kolli@heits.in>
2024-11-24 21:18:50 +0530

    Add project files.

---
+++ Hubs/BoardHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Whiteboard.Hubs;
+
+public class BoardHub : Hub
+{
+    public async Task DrawingAsync(int startX, int startY, int endX, int endY)
+    {
+        // Broadcasts the coordinates to all clients except the sender
+        await Clients.Others.SendAsync("OnDrawingAsync", startX, startY, endX, endY);
+    }
+}
